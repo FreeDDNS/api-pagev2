0ea0ab23e4721ade12b7b8b5ad483fc9ad30e363
Muhammad Rifky Abimayu <34646726+rifkytech@users.noreply.github.com>
2018-10-20 14:35:54 +0700

    Rapikan Baris Hehehe :)

--- dist/lib/proses.php
+++ dist/lib/proses.php
@@ -18,24 +18,24 @@ $responseKeys	= json_decode($response,true);
     if(intval($responseKeys["success"]) !== 1) {
       header("Location: ../../index.php?status=1&message= Mohon Centang Form Recaptcha !");
    } else {
-$api_url    	= "https://panel.hostddns.us/api/v2/client/"; // Stable API Url v2 
-$api_key    	= $json['data']['api_key']; 
-$service    	= $_POST['service']; 
-$domain     	= $_POST['domain']; 
-$subdomain  	= $_POST['subdomain']; 
-$content    	= $_POST['content']; 
-$ch         	= curl_init();
+$api_url    		= "https://panel.hostddns.us/api/v2/client/"; // Stable API Url v2 
+$api_key    		= $json['data']['api_key']; 
+$service    		= $_POST['service']; 
+$domain     		= $_POST['domain']; 
+$subdomain  		= $_POST['subdomain']; 
+$content    		= $_POST['content']; 
+$ch         		= curl_init();
 curl_setopt($ch, CURLOPT_URL, "$api_url?key=$api_key&service=$service&domain=$domain&subdomain=$subdomain&content=$content");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_VERBOSE, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-$result     	= curl_exec($ch);
+$result     		= curl_exec($ch);
 $newarray 		= json_decode(trim($result), TRUE);
 $status 		= $newarray["success"];
 $message 		= $newarray["error"]["message"];
 $data_type 		= $newarray['data']['type'];
-$data_subdo 	= $newarray['data']['subdomain'];
-$data_content 	= $newarray['data']['content'];
+$data_subdo 		= $newarray['data']['subdomain'];
+$data_content 		= $newarray['data']['content'];
 $data_pin 		= $newarray['data']['pin'];
 if($status == 'false'){
 	header("Location: ../../index.php?status=1&message=   $message ");
